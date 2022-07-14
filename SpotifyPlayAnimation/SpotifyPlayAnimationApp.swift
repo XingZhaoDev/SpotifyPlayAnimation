e45b928dafb9488bcfcd7dad153343d31d61ce9c
Xing Zhao <zhaoxing8325@gmail.com>
2022-07-13 21:02:00 -0700

    Initial Commit

---
+++ SpotifyPlayAnimation/SpotifyPlayAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyPlayAnimationApp.swift
+//  SpotifyPlayAnimation
+//
+//  Created by Xing Zhao on 2022-07-13.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyPlayAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpotifyPlayAnimationUITests/SpotifyPlayAnimationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpotifyPlayAnimationUITestsLaunchTests.swift
+//  SpotifyPlayAnimationUITests
+//
+//  Created by Xing Zhao on 2022-07-13.
+//
+
+import XCTest
+
+final class SpotifyPlayAnimationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
